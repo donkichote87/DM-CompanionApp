0de56379c2e1a614b4c1fa12bcd56c303322cd01
[Kamil Lazarowicz] <[kamil_lazarowicz@hotmail.com]>
2021-07-20 20:47:58 +0000

    Bootstrap added with some basic theme, some tweaks with views

---
+++ src/main/java/pl/basicstuff/dmcompanionapp/attributes/Attributes.java
@@ -0,0 +1,24 @@
+package pl.basicstuff.dmcompanionapp.attributes;
+
+
+
+public class Attributes {
+
+    private String characterClass;
+    private int level;
+    private String alignment;
+    private int hitPoints;
+    private int speed;
+    private int str;
+    private int dex;
+    private int con;
+    private int anInt;
+    private int wis;
+    private int cha;
+    private String[] skills;
+    private String senses;
+    private String[] languages;
+    private String[] possessions;
+
+
+}
