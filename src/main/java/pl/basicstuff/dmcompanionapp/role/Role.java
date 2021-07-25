08338fd85300fbac37400acdef1b7b0d7daf6ddd
[Kamil Lazarowicz] <[kamil_lazarowicz@hotmail.com]>
2021-07-25 22:43:38 +0000

    properties and pom update before heroku

--- src/main/java/pl/basicstuff/dmcompanionapp/role/Role.java
+++ src/main/java/pl/basicstuff/dmcompanionapp/role/Role.java
@@ -1,9 +1,6 @@
 package pl.basicstuff.dmcompanionapp.role;
 
 import lombok.Data;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.basicstuff.dmcompanionapp.npc.Npc;
 
 import javax.persistence.*;
 
