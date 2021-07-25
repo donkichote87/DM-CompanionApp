b75a54827a67fd02713a3f717f2f16427766ab7c
[Kamil Lazarowicz] <[kamil_lazarowicz@hotmail.com]>
2021-07-25 22:24:45 +0000

    properties update before heroku

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `roles` VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+INSERT INTO `users` VALUES (1,1,'$2a$10$FLjiKvO9AdMRbaCbcSMsR.IpzXkly/MthxEHq1ENeq/VfMEC9/Cxy','admin',2,'',NULL);
+
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `enabled` int NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(60) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `email` varchar(60) NOT NULL,
+  `verification_code` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`),
+  UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`),
+  KEY `FKp56c1712k691lhsyewcssf40f` (`role_id`),
+  CONSTRAINT `FKp56c1712k691lhsyewcssf40f` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
