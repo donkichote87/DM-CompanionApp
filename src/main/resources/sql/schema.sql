650713c3bd6dc34bb11065019bb2e878230a1788
[Kamil Lazarowicz] <[kamil_lazarowicz@hotmail.com]>
2021-07-25 23:02:47 +0000

    properties and pom update before heroku

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,11 +1,11 @@
 
-CREATE TABLE `roles` (
+CREATE TABLE IF NOT EXISTS `roles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `enabled` int NOT NULL,
   `password` varchar(255) DEFAULT NULL,
