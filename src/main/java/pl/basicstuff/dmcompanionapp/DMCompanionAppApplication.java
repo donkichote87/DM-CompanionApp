caa1b0aec10b5d7216a2af175020e7370b4de5d7
donkichote87 <kamil_lazarowicz@hotmail.com>
2022-04-02 15:36:52 +0200

    AWS deployment

--- src/main/java/pl/basicstuff/dmcompanionapp/DMCompanionAppApplication.java
+++ src/main/java/pl/basicstuff/dmcompanionapp/DMCompanionAppApplication.java
@@ -2,9 +2,16 @@ package pl.basicstuff.dmcompanionapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DMCompanionAppApplication {
+public class DMCompanionAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DMCompanionAppApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DMCompanionAppApplication.class, args);
